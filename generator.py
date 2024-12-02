115b55152671f5f482f56bbb10316602788eb571
cassiama <chanceyassiamah@gmail.com>
2024-12-02 02:07:22 -0500

    Printing the input file has been redone to remove bugs, added a small amount of mf_struct stuff (subject to change)

--- generator.py
+++ generator.py
@@ -15,32 +15,50 @@ def main():
         ere = f.read().splitlines()
 
     mf_struct = {}
-    print(ere)
-    for i, l in enumerate(ere):
-        if l == "SELECT ATTRIBUTE(S):":
-            select_attr = ere[i+1].split(",")
-            print(f"S: {select_attr}")
-        if l == "NUMBER OF GROUPING VARIABLES(n):":
-            n = int(ere[i+1])
-            print(f"n: {n}")
-        if l == "GROUPING ATTRIBUTES(V):":
-            group_attr = ere[i+1].split(",")
-            print(f"V: {group_attr}")
-        if l == "F-VECT([F]):":
-            aggregates = ere[i+1].split(",")
-            print(f"F: {aggregates}")
-        if l == "SELECT CONDITION-VECT([C]):":
-            select_conds = []
-            curr_line = l
-            while curr_line != "HAVING CLAUSE (G):":
-                i += 1
-                curr_line = ere[i+1]
-                select_conds.append(curr_line)
-            print(f"C: {select_conds}")
-        # if l == "HAVING CLAUSE (G):":
-        #     having_conds = []
-        #     curr_line = l
-        #     while curr_line != "":
+    # print(ere)
+    n_gv = 0
+    select_attr = group_attr = aggregates = select_conds = having_conds = []
+
+    # get the select attributes
+    select_attr = ere[ere.index("SELECT ATTRIBUTE(S):") + 1].split(", ")
+    print(f"S: {select_attr}")
+    for attr in select_attr:
+        mf_struct[attr] = []
+    # mf_struct['S'] = select_attr
+    # uncomment line above if we decide to handle mf_struct differently
+
+    # get the number of grouping variables
+    n_gv = int(ere[ere.index("NUMBER OF GROUPING VARIABLES(n):") + 1])
+    print(f"n: {n_gv}")
+    # mf_struct['n'] = n_gv
+    # uncomment line above if we decide to handle mf_struct differently
+
+    # get the grouping attributes
+    group_attr = ere[ere.index("GROUPING ATTRIBUTES(V):") + 1].split(', ')
+    print(f"V: {group_attr}")
+    # mf_struct['V'] = group_attr
+    # uncomment line above if we decide to handle mf_struct differently
+
+    # get the aggregates vector
+    aggregates = ere[ere.index("F-VECT([F]):") + 1].split(', ')
+    print(f"F: {aggregates}")
+    # mf_struct['F'] = aggregates
+    # uncomment line above if we decide to handle mf_struct differently
+
+    # get the select condition vector
+    select_conds = ere[ere.index("SELECT CONDITION-VECT([C]):") + 1:ere.index("HAVING CLAUSE (G):")]
+    print(f"C: {select_conds}")
+    # mf_struct['C'] = select_conds
+    # uncomment line above if we decide to handle mf_struct differently
+
+    # get the having clause vector (if it exists)
+    having_conds = ere[ere.index("HAVING CLAUSE (G):") + 1:]
+    if len(having_conds) >= 1 and having_conds[0] != '-':
+        print(f"G: {having_conds}")
+    # mf_struct['G'] = having_conds
+    # uncomment line above if we decide to handle mf_struct differently
+
+    print(f"Columns of mf_struct: {list(mf_struct.keys())}")
 
 
     '''
