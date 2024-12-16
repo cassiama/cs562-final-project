e7e13ec9b4ab29a03b20593220c8a7e3500f2e08
cassiama <chanceyassiamah@gmail.com>
2024-12-16 08:13:23 -0500

    fixed "psycopg2 not found" error (bug might have only been on my machine, but it should still work without venv)

--- generator.py
+++ generator.py
@@ -1,4 +1,5 @@
 import subprocess
+import sys
 
 def main():
     """
@@ -231,7 +232,7 @@ if "__main__" == __name__:
     # Write the generated code to a file
     open("_generated.py", "w").write(tmp)
     # Execute the generated code
-    subprocess.run(["python", "_generated.py"])
+    subprocess.run([sys.executable, "_generated.py"])
 
 
 if "__main__" == __name__:
